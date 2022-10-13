ab0ba032d9ed11dc374919cb0013757dc3a56080
Maryam Verdon <maryamverdon@gmail.com>
2022-10-13 16:36:56 +0200

    Create Tasks

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,4 +22,6 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+Route::resource('tasks', TaskController::class)->middleware('auth');
+
+require __DIR__ . '/auth.php';
